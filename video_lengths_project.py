73ddb64d3c9d211ca80155958dce8aae59387800
jithendra <mittajithendra46@gmail.com>
2019-06-10 16:05:34 +0530

    Update video_lengths_project.py

--- video_lengths_project.py
+++ video_lengths_project.py
@@ -6,13 +6,11 @@ import time
 t1=time.time()
 p=Path(input()).resolve()
 p=str(p)
-os.chdir(p)
-
-
-total=0
 
 
 if os.path.isdir(p):
+    os.chdir(p)
+    total=0
     for root,dirs,files in os.walk(p):
         for file in files:
             if file.endswith('.mp4'):
@@ -24,14 +22,17 @@ if os.path.isdir(p):
                     clip.audio.reader.close_proc()
                 except:
                     print("Invalid File"+file)
+
+
+    t2=time.time()
+    print("Total lengtgh of the videos")
+    print(total,end=" ")
+    print("seconds")
+    print(t2-t1,end=" ")
+    print("seconds taken to execute program")
+
 else:
     print("path was not found")
-t2=time.time()
-print("Total lengtgh of the videos")
-print(total,end=" ")
-print("seconds")
-print(t2-t1,end=" ")
-print("seconds taken to execute program")
 
 
 
